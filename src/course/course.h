e32a39e836dba67af57e371944c97140f642f9fa
Haseeb Ahmed <25-cs-066@student.hitecuni.edu.pk>
2026-06-22 20:27:58 +0500

    debugged

--- src/course/course.h
+++ src/course/course.h
@@ -73,10 +73,13 @@ public:
 	}
 	void enrollstudent(student* s)
 	{
-		if (enrolledcount >= maxcapacity)
+		if (enrolledcount < maxcapacity)
 		{
 			enrolledstudents[enrolledcount] = s;
 			enrolledcount++;
+		}
+		else
+		{
 			throw capacityexceededexception();
 		}
 		
@@ -122,60 +125,18 @@ private:
 	char grade;
 
 public:
-	enrollment(atudent* s, course* c, string date, char g)
+	enrollment(student* s, course* c, string date, char g)
 	{
 		enrolledstudent = s;
 		enrolledcourse = c;
 		enrollmentdate = date;
 		grade = g;
 	}
-	void dispaly()
+	void display()
 	{
 
 		cout << "enrollement date " << enrollmentdate << endl;
-		cout << "grade" << endl;
+		cout << "grade" <<grade << endl;
 	}
 };
-int main()
-{
-	string facultycourses[3] = {"oop","pf","dld"};
-	faculty f1("naveed", "1232-34534", 50,"348635345","f1014","computer science","prof", facultycourses);
-
-	string studentcourses[3] = { "oop","dsa","dbms" };
-	student s1("haseeb","111111-1111",20 ,"345875","25-cs-066",4 ,3.8 , studentcourses);
-
-	student s2("irfan", "234324324-45", 20, "3584635", "25-cs-131", 4, 3.0, studentcourses);
-
-	course c1("cs201", "oop", 3, &f1, 2);
-	course c2("cs201", "oop", 3, &f1, 5);
-
-
-	if (c1 == c2)
-	{
-		cout << "courses are same" << endl;
-	}
-
-	try
-	{
-		c1.enrollstudent(&s1);
-		c1.enrollstudent(&s2);
-
-		student s3("Hassan","33333-3333333-3",20,"03003334444","23-CS-003",4,3.2,studentcourses);
-
-		c1.enrollstudent(&s3);
-
-	}
-	catch (capacityexceededexception e)
-	{
-		e.message();
-	}
-	cout << c1;
-
-	student** waitinglist;
-	waitinglist = c1 + c2;
-
-	enrollment e1(&s1, &c1, "20-06-2020", 'A');
-	e1.display();
-	return 0;
-}
 #endif
